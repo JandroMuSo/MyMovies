734d98ab0241356459a7c272202f5e74488e13ad
JandroMuSo <jandromuso@gmail.com>
2021-02-10 22:38:23 +0100

    Creación de la obtención de peliculas por código de region usando el los servicios de location de google play

--- app/src/main/java/com/jandromuso/mymovies/ui/main/MainActivity.kt
+++ app/src/main/java/com/jandromuso/mymovies/ui/main/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.jandromuso.mymovies.ui.main
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.jandromuso.mymovies.ui.detail.DetailActivity
 import com.jandromuso.mymovies.R
 import com.jandromuso.mymovies.client.MovieDbClient
@@ -16,13 +21,16 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    companion object{
+        private const val DEFAULT_REGION = "US"
+    }
+
+    private lateinit var fusedLocationClient :  FusedLocationProviderClient
+
+    private val moviesAdapter = MoviesAdapter(emptyList()) { navigateTo(it) }
+
     private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ isGranted ->
-            val message = when {
-                isGranted -> "Permission Granted"
-                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) -> "Should show Rationale"
-                else -> "Permission Rejected"
-            }
-            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        requestPopularMovies(isGranted)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,18 +38,44 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val moviesAdapter = MoviesAdapter(emptyList()) { navigateTo(it) }
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
         binding.recycler.adapter = moviesAdapter
         requestPermissionLauncher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
 
+
+
+    }
+    @SuppressLint("MissingPermission")
+    private fun requestPopularMovies(isLocationGranted: Boolean){
+        if(isLocationGranted){
+            fusedLocationClient.lastLocation.addOnCompleteListener{
+                doRequestPopularMovies(getRegionFromLocation(it.result))
+            }
+        }else{
+            doRequestPopularMovies(DEFAULT_REGION)
+        }
+
+    }
+
+    private fun doRequestPopularMovies(region: String) {
         lifecycleScope.launch{
             val apiKey = getString(R.string.api_key)
-            val popularMovies = MovieDbClient.service.listPopularMovies(apiKey)
+            val popularMovies = MovieDbClient.service.listPopularMovies(apiKey, region)
             moviesAdapter.movies = popularMovies.results
             //Esto indica al adapter que tiene que rerenderizar la vista
             moviesAdapter.notifyDataSetChanged()
         }
+    }
 
+    private fun getRegionFromLocation(location: Location?) : String{
+        if(location==null){
+            return DEFAULT_REGION
+        }
+        val geocoder = Geocoder(this)
+        val result = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+        return result.firstOrNull()?.countryCode ?: DEFAULT_REGION
     }
 
     private fun navigateTo(movie: Movie) {
